7b640434d8505afd314ffb603c3ec00a6b91b6ef
Lia <liacicati@gmail.com>
2021-04-06 15:20:41 +0200

    feat : prepared directories

---
+++ Client/src/Main.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Application.launch(MyApplication.class);
+  }
+}
---
+++ Server/src/MyApplication.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyApplication extends Application
+{
+  @Override public void start(Stage stage) throws IOException
+  {
+
+  }
+}
