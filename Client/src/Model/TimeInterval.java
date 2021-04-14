b26b1718701806b2cb161ed898c40f82546c3b49
Lori <loredanacicati@gmail.com>
2021-04-14 14:33:21 +0200

    added main functionality to TimeInterval class

--- Client/src/Model/TimeInterval.java
+++ Client/src/Model/TimeInterval.java
@@ -2,4 +2,32 @@ package Model;
 
 public class TimeInterval
 {
+  private MyDateTime startTime;
+  private MyDateTime endTime;
+
+  public TimeInterval(MyDateTime startTime,MyDateTime endTime)
+  {
+    setStartTime(startTime);
+    setEndTime(endTime);
+  }
+
+  public MyDateTime getStartTime()
+  {
+    return startTime;
+  }
+
+  public void setStartTime(MyDateTime startTime)
+  {
+    this.startTime = startTime;
+  }
+
+  public MyDateTime getEndTime()
+  {
+    return endTime;
+  }
+
+  public void setEndTime(MyDateTime endTime)
+  {
+    this.endTime = endTime;
+  }
 }
