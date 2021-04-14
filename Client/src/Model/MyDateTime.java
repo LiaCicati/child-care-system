118f6c4de18e6b132f6b0d6b4c40d5311347b84d
Christine Grabow <christine-m-g@hotmail.com>
2021-04-14 13:12:17 +0200

    Added the bare minimum to be able to calculate the age of kid

--- Client/src/Model/MyDateTime.java
+++ Client/src/Model/MyDateTime.java
@@ -2,4 +2,37 @@ package Model;
 
 public class MyDateTime
 {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDateTime(int day, int month, int year){
+        this.setDay(day);
+        this.setMonth(month);
+        this.setYear(year);
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
