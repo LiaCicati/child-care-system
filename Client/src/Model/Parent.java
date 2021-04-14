991f13783a01c7ba8f7bbfba72bf36632c129209
Christine Grabow <christine-m-g@hotmail.com>
2021-04-14 12:42:09 +0200

    Added main content of parent class

--- Client/src/Model/Parent.java
+++ Client/src/Model/Parent.java
@@ -1,5 +1,39 @@
 package Model;
+import java.util.ArrayList;
+
 
 public class Parent
 {
+    private int nrOfKids;
+    private boolean hasPets;
+    private ArrayList<Kid> kids;
+
+    public Parent(boolean hasPets){
+        this.setHasPets(hasPets);
+    }
+
+
+    public int getNrOfKids() {
+        return kids.size();
+    }
+
+    public void setNrOfKids(int nrOfKids) {
+        this.nrOfKids = nrOfKids;
+    }
+
+    public boolean isHasPets() {
+        return hasPets;
+    }
+
+    public void setHasPets(boolean hasPets) {
+        this.hasPets = hasPets;
+    }
+
+    public ArrayList<Kid> getKids() {
+        return kids;
+    }
+
+    public void addKid(Kid kid) {
+        this.kids = kids;
+    }
 }
